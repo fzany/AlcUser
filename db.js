7fca7b893741d3778bc920a6224d0b7c475331b3
Olorunfemi Ajibulu <Olorunfemi Ajibulu>
2017-12-22 15:02:56 +0100

    change db url

--- db.js
+++ db.js
@@ -1,2 +1,10 @@
-var mongoose = require('mongoose');
-mongoose.connect(process.env.DB_URL, { useMongoClient: true });
+var mongoose = require("mongodb").MongoClient;
+mongoose.connect("mongodb://fzanymongol:Cwg8ErXXvzOgdKCkyIb71lIv5ZMxteKMaPr1sb2oD2PbjTOAaDoPUjrlkhUQFaFR5L9Yo59MO3NzgpT6pIsUvQ==@fzanymongol.documents.azure.com:10255/?ssl=true", function (err, db) {
+  db.close();
+});
+
+
+
+/*var mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URL, { useMongoClient: true });*/
+
